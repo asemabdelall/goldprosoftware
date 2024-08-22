810475fbb2b6d3aef5bd8c69412980d3c558b01f
asemabdelall <osama.goldpro.kw@gmail.com>
2024-08-22 13:00:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -128,6 +128,18 @@ app.get('/ve', (req, res) => {
     });
 });
 
+app.get('/kit', (req, res) => {
+    const sql = "SELECT * FROM productss WHERE type = 'kit'";
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.error('Error executing query: ', err);
+            return res.status(500).json({ error: err.message });
+        }
+        console.log('Data retrieved for /kit: ', data);
+        return res.json(data);
+    });
+});
+
 app.listen(8081, () => {
     console.log("Listening on port 8081");
 });
